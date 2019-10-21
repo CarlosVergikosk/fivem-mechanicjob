68e107caa466d568e246fec5960d72bb56378a88
CarlosVergikosk <45525595+CarlosVergikosk@users.noreply.github.com>
2019-10-21 22:21:49 +0100

    Added the usable items

--- inrp_mechanicjob/server/main.lua
+++ inrp_mechanicjob/server/main.lua
@@ -10,10 +10,6 @@ local Vehicles = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-if Config.MaxInService ~= -1 then
-	TriggerEvent('esx_service:activateService', 'mechanic', Config.MaxInService)
-end
-
 TriggerEvent('esx_phone:registerNumber', 'mechanic', _U('mechanic'), true, true)
 TriggerEvent('esx_society:registerSociety', 'mechanic', 'mechanic', 'society_mechanic', 'society_mechanic', 'society_mechanic', {type = 'public'})
 
@@ -313,3 +309,34 @@ ESX.RegisterServerCallback('esx_mechanicjob:getVehiclesPrices', function(source,
 		cb(Vehicles)
 	end
 end)
+
+ESX.RegisterUsableItem('blowpipe', function(source)
+	local _source = source
+	local xPlayer  = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('blowpipe', 1)
+
+	TriggerClientEvent('esx_mechanicjob:onHijack', _source)
+	TriggerClientEvent('esx:showNotification', _source, _U('you_used_blowtorch'))
+end)
+
+ESX.RegisterUsableItem('fixkit', function(source)
+	local _source = source
+	local xPlayer  = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('fixkit', 1)
+
+	TriggerClientEvent('esx_mechanicjob:onFixkit', _source)
+	TriggerClientEvent('esx:showNotification', _source, _U('you_used_repair_kit'))
+end)
+
+ESX.RegisterUsableItem('carokit', function(source)
+	local _source = source
+	local xPlayer  = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('carokit', 1)
+
+	TriggerClientEvent('esx_mechanicjob:onCarokit', _source)
+	TriggerClientEvent('esx:showNotification', _source, _U('you_used_body_kit'))
+end)
+
